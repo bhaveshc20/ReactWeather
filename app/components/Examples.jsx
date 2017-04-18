f014222a80bbeeeec38f9817bec8e6a3d7c3ac39
bhaveshc20 <bhaveshc789@gmail.com>
2017-04-17 22:24:47 -0700

    Initial commit

---
+++ app/components/Examples.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+// var Examples = React.createClass({
+//   render: function () {
+//     return (
+//       <h3>Examples component</h3>
+//     )
+//   }
+// });
+var Examples = (props) => {
+  return (
+    <h3>Example component</h3>
+  )
+};
+module.exports = Examples;

9549f985709c69c4889d05089c2753fe9df96a2b
bhaveshc20 <bhaveshc789@gmail.com>
2017-04-18 17:34:21 -0700

    Rename about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,11 @@ var React = require('react');
 
 var About = (props) => {
   return(
-    <h3>About component</h3>
+    <div>
+        <h3>About component</h3>
+        <p>Welcome to about page!</p>
+    </div>
+
   )
 };
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -9,7 +9,7 @@ var React = require('react');
 // });
 var Examples = (props) => {
   return (
-    <div><h3>Examples</h3>
+    <div><h3>Example</h3>
     <p>Welcome to example page!</p>
     </div>
 

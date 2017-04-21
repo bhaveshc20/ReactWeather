4334f8c5b68dc4a1f8ae052eec6e94991a089269
bhaveshc20 <bhaveshc789@gmail.com>
2017-04-20 20:03:50 -0700

    styling about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,17 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-        <h3>About component</h3>
-        <p>Welcome to about page!</p>
+        <h1 className="text-center">About component</h1>
+        <p>A web app build on react. I have built this for the complete React web app developer course</p>
+        <p>Here are some tools that I used:</p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> -This was the javascript framework used
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open weather map</a>
+          </li>
+        </ul>
     </div>
 
   )
